f523eb166a17a80d7f250ea5c2a3e59c6f46ab7c
kcj1805 <cjk1805@naver.com>
2021-04-05 23:56:15 +0900

    가장큰수, K 번째 수

---
+++ Programmers/KNumber.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.io.InputStreamReader;
+
+public class KNumber {
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
+        //String cnt = br.readLine();
+        //String input = br.readLine();
+        int[] in = {1, 5, 2, 6, 3, 7, 4};
+        int[][] com = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        
+        int[] out = Solution(in,com);
+
+        wr.write("");
+        
+        wr.flush();
+        wr.close();
+        br.close();
+    }
+    public static int[] Solution(int[] array, int[][] commands)
+    {
+        int[] answer = new int[commands.length];
+
+        for(int a=0;a<commands.length;a++)
+        {
+            int i=commands[a][0];
+            int j=commands[a][1];
+            int k=commands[a][2];
+            int[] cutarray= new int[j-i+1];
+            //cutarray = Arrays.copyOfRange(array, i-1, j);
+            for(int b=0;b<cutarray.length;b++)
+            {
+                cutarray[b]=array[i++-1];
+            }
+            Arrays.sort(cutarray);
+            answer[a] = cutarray[k-1];
+        }
+        return answer;
+    }
+}
---
+++ Programmers/LargestNumber.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+
+public class LargestNumber {
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
+        //String cnt = br.readLine();
+        //String input = br.readLine();
+
+        int[] input = {6, 10, 2};
+        Solution(input);
+        wr.write("");
+        
+        wr.flush();
+        wr.close();
+        br.close();
+    }
+    public static String Solution(int[] numbers)
+    {
+        // 숫자를 문자열로 변환
+		String[] result = new String[numbers.length];
+		for (int i = 0; i < numbers.length; i++) {
+			result[i] = String.valueOf(numbers[i]);
+		}
+
+		// 정렬
+		//Arrays.sort(result, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        Arrays.sort(result, new Comparator<String>() {
+
+			@Override
+			public int compare(String o1, String o2) {
+
+				return ((o2 + o1).compareTo(o1 + o2));
+			}
+		});
+		// 0만 여러개 있는 배열의 경우 하나의 0만 리턴
+		if (result[0].equals("0")) {
+			return "0";
+		}
+
+		String answer = "";
+		// 정렬된 문자 하나로 합치기
+		for (String a : result) {
+			answer += a;
+		}
+		return answer;
+    }
+}
