881aaa0ac7eb153a64b25bd45984c10684c089a0
kcj1805 <cjk1805@naver.com>
2021-04-11 10:22:35 +0900

    주석추가

--- BOJ/BOJ16564.java
+++ BOJ/BOJ16564.java
@@ -28,10 +28,12 @@ public class BOJ16564 {
         int answer=1;
         int low = 1;
         int high = 100000000;
+        //이진 탐색
         while(low<=high)
         {
             int mid= (high+low)/2;
             long sum = 0;
+            //중간레벨에 맞춰서 레벨업 횟수 추가
             for(int i=0;i<level.length;i++)
             {
                 if(mid-level[i]>0)
@@ -39,10 +41,12 @@ public class BOJ16564 {
                     sum += mid-level[i];
                 }
             }
+            //주어진 횟수보다 높을경우 왼쪽반(?)으로 재계산
             if(sum>K)
             {
                 high = mid-1;
             }
+            //오른쪽반(?)으로 재계산
             else
             {
                 answer = Math.max(answer, mid);
