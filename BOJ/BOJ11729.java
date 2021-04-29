3077eeceb4052c10d85133d4a2f6cda904723ae0
cjkim <cjkim@unitech.co.kr>
2021-04-29 18:34:54 +0900

    하노이탑

--- BOJ/BOJ11729.java
+++ BOJ/BOJ11729.java
@@ -6,26 +6,28 @@ public class BOJ11729 {
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
-        //String input = br.readLine();
+        String input = br.readLine();
 
-        Solution(3);
-        wr.write("");
-        
+        int N = Integer.parseInt(input);
+        int cnt = (int)(Math.pow(2,N)-1);
+        wr.write(Integer.toString(cnt)+"\n");
+        Calc(N,1,2,3);
+
+        for (String string : strList) {
+            wr.write(string + "\n");
+        }
         wr.flush();
         wr.close();
         br.close();
     }
     static List<String> strList = new ArrayList<>();
-    public static int Solution(int input)
+    public static void Calc(int N, int from, int mid, int to)
     {
-        int answer=0;
-        
-        Calc(input,3);
-
-        return answer;
-    }
-    public static void Calc(int N, int to)
-    {
-        
+        if(N!=0)
+        {
+            Calc(N-1,from,to,mid);
+            strList.add(from + " " + to);
+            Calc(N-1,mid,from,to);
+        }
     }
 }
