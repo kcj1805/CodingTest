90a347c090e51679fecd71a5cb80c6e72ccc39bd
kcj1805 <cjk1805@naver.com>
2021-05-27 01:04:38 +0900

    N-Queen 중복제거 해야됨

--- BOJ/BOJ9663.java
+++ BOJ/BOJ9663.java
@@ -8,19 +8,44 @@ public class BOJ9663 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int[][] Chess = new int[N][N];
-        Solution(Chess, N, 0, 0, 0);
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                Solution(Chess, N, i, j, 0);
+                Chess = new int[N][N];
+            }
+        }
         System.out.println(answer);
         sc.close();
     }
-    public static boolean Solution(int[][] Chess, int N, int x, int y, int cnt)
+    public static void Solution(int[][] Chess, int N, int x, int y, int cnt)
     {
-        int[][] ChessCopy = new int[N][N];
         Chess[x][y]=2;
         cnt++;
         if(cnt==N){
             answer++;
-            return false;
+            return;
         }
+        if(!BlockChess(Chess, N, x, y)){
+            return;
+        }
+        int[][] ChessCopy = new int[N][N];
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                if(Chess[i][j]==0){
+                    ArrCopy(ChessCopy,Chess);
+                    Solution(ChessCopy, N, i, j, cnt);
+                }
+            }
+        }
+    }
+    public static void ArrCopy(int[][] des, int[][] sour){
+        for(int i=0;i<des.length;i++){
+            for(int j=0;j<des.length;j++){
+                des[i][j]=sour[i][j];
+            }
+        }
+    }
+    public static boolean BlockChess(int[][] Chess, int N, int x, int y){
         for(int i=0;i<N;i++){
             for(int j=0;j<N;j++){
                 if(!(i==x && j==y)){
@@ -35,23 +60,39 @@ public class BOJ9663 {
                 }
             }
         }
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                if(Chess[i][j]==0){
-                    ArrCopy(ChessCopy,Chess);
-                    Solution(ChessCopy, N, i, j, cnt);
-                    //ArrCopy(Chess,ChessCopy);
-                    cnt--;
-                    Chess[i][j]=0;
+        int i=1;
+        while(true){
+            //좌상
+            if(x-i>=0&&y-i>=0){
+                if(Chess[x-i][y-i]==2){
+                    return false;
                 }
+                Chess[x-i][y-i]=1;
             }
-        }
-        return true;
-    }
-    public static void ArrCopy(int[][] des, int[][] sour){
-        for(int i=0;i<des.length;i++){
-            for(int j=0;j<des.length;j++){
-                des[i][j]=sour[i][j];
+            //좌하
+            if(x+i<N&&y-i>=0){
+                if(Chess[x+i][y-i]==2){
+                    return false;
+                }
+                Chess[x+i][y-i]=1;
+            }
+            //우상
+            if(x-i>=0&&y+i<N){
+                if(Chess[x-i][y+i]==2){
+                    return false;
+                }
+                Chess[x-i][y+i]=1;
+            }
+            //우하
+            if(x+i<N&&y+i<N){
+                if(Chess[x+i][y+i]==2){
+                    return false;
+                }
+                Chess[x+i][y+i]=1;
+            }
+            i++;
+            if(i==N){
+                return true;
             }
         }
     }
