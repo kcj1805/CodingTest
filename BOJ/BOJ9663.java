e98eb651d92afb47282fda62375247dbdc934701
cjkim <cjkim@unitech.co.kr>
2021-06-14 09:39:57 +0900

    N-queen

--- BOJ/BOJ9663.java
+++ BOJ/BOJ9663.java
@@ -3,97 +3,42 @@ import java.util.*;
 
 public class BOJ9663 {
     //N-Queen
-    static int answer=0;
+    static int N, ans;
+
+    static boolean[] col, slash, backslash;
+
     public static void main(String[] args) throws IOException
     {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[][] Chess = new int[N][N];
-        int[][] ChessCopy = new int[N][N];
-        for(int i=0;i<N;i++){
-            ArrCopy(Chess, ChessCopy);
-            Solution(Chess, N, 0, i, 0);
-        }
-        System.out.println(answer);
+
+        N = sc.nextInt();
+
+        col = new boolean[N];
+        slash = new boolean[2 * N - 1];
+        backslash = new boolean[2 * N - 1];
+
+        ans = 0;
+        solve(0);
+        System.out.println(ans);
         sc.close();
     }
-    public static void Solution(int[][] Chess, int N, int x, int y, int cnt)
-    {
-        int[][] ChessCopy = new int[N][N];
-        Chess[x][y]=2;
-        cnt++;
-        if(cnt==N){
-            answer++;
+
+    public static void solve(int r) {
+        if (r == N) {
+            ans++;
             return;
         }
-        if(!BlockChess(Chess, N, x, y)){
-            return;
-        }
-        for(int i=x;i<N;i++){
-            for(int j=0;j<N;j++){
-                if(Chess[i][j]==0){
-                    ArrCopy(ChessCopy,Chess);
-                    Solution(ChessCopy, N, i, j, cnt);
-                }
+        for (int c = 0; c < N; c++) {
+            if (check(r, c)) {
+                col[c] = slash[r + c] = backslash[r - c + N - 1] = true;
+                solve(r + 1);
+                col[c] = slash[r + c] = backslash[r - c + N - 1] = false;
             }
         }
     }
-    public static void ArrCopy(int[][] des, int[][] sour){
-        for(int i=0;i<des.length;i++){
-            for(int j=0;j<des.length;j++){
-                des[i][j]=sour[i][j];
-            }
-        }
-    }
-    public static boolean BlockChess(int[][] Chess, int N, int x, int y){
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                if(!(i==x && j==y)){
-                    if(i==x || j==y){
-                        if(Chess[i][j]!=2){
-                            Chess[i][j]=1;
-                        }
-                        else{
-                            return false;
-                        }
-                    }
-                }
-            }
-        }
-        int i=1;
-        while(true){
-            //좌상
-            if(x-i>=0&&y-i>=0){
-                if(Chess[x-i][y-i]==2){
-                    return false;
-                }
-                Chess[x-i][y-i]=1;
-            }
-            //좌하
-            if(x+i<N&&y-i>=0){
-                if(Chess[x+i][y-i]==2){
-                    return false;
-                }
-                Chess[x+i][y-i]=1;
-            }
-            //우상
-            if(x-i>=0&&y+i<N){
-                if(Chess[x-i][y+i]==2){
-                    return false;
-                }
-                Chess[x-i][y+i]=1;
-            }
-            //우하
-            if(x+i<N&&y+i<N){
-                if(Chess[x+i][y+i]==2){
-                    return false;
-                }
-                Chess[x+i][y+i]=1;
-            }
-            i++;
-            if(i==N){
-                return true;
-            }
-        }
+    public static boolean check(int r, int c) {
+        if (col[c] || slash[r + c] || backslash[r - c + N - 1])
+            return false;
+        return true;
     }
 }
