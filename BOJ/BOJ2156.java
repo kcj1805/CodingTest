7ffcb646fcfa501a8360cc67066b1812ed14a211
cjkim <cjkim@unitech.co.kr>
2021-06-29 09:57:24 +0900

    포도주시식

--- BOJ/BOJ2156.java
+++ BOJ/BOJ2156.java
@@ -2,14 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ2156 {
-    //6
-    //1000
-    //1000
-    //1
-    //1
-    //1000
-    //1000
-
+    //포도주 시식
     public static void main(String[] args) throws IOException
     {
         Scanner sc = new Scanner(System.in);
@@ -24,16 +17,19 @@ public class BOJ2156 {
 
         for(int i=0;i<N;i++){
             if(i==0){
-                dp[i] = wine[i];
+                dp[i] = wine[0];
             }
             else if(i==1){
-                dp[i] = dp[i-1]+wine[i];
+                dp[i] = dp[0]+wine[1];
             }
             else if(i==2){
-                dp[i] = Math.max(wine[i-2]+wine[i], wine[i-1]+wine[i]);
+                dp[i] = Math.max(wine[0]+wine[2], wine[1]+wine[2]);
+            }
+            else if(i==3){
+                dp[i] = Math.max(wine[0]+wine[1]+wine[3], wine[0]+wine[2]+wine[3]);
             }
             else{
-                dp[i] = Math.max(Math.max(dp[i-2]+wine[i],dp[i-3]+wine[i-1]+wine[i]),dp[i-3]+wine[i]);
+                dp[i] = Math.max(Math.max(dp[i-2]+wine[i], dp[i-3]+wine[i-1]+wine[i]), dp[i-4]+wine[i-1]+wine[i]);
             }
             answer = Math.max(answer,dp[i]);
         }
