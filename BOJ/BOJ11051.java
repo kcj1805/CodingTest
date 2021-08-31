421519e94d7473a66be93554a746ed22cbf4f8dc
cjkim <cjkim@unitech.co.kr>
2021-08-31 18:26:18 +0900

    링, 이항계수1, 이항계수2(미완)

---
+++ BOJ/BOJ11051.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ11051 {
+    //이항계수 2
+    static int[][] dp = new int[1001][1001];
+    public static void main(String[] args) throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int answer = Comb(N,K);
+        System.out.println(answer);
+        sc.close();
+    }
+    public static int Comb(int N, int K)
+    {
+        if(N<=0 || K<=0){
+            dp[N][K]=1;
+        }
+        else if(dp[N][K] == 0){
+            dp[N][K] = Comb(N-1, K-1) + Comb(N-1, K);
+        }
+        return dp[N][K];
+    }
+}
