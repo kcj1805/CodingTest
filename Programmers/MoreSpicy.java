f5e6e7c90c9d509adaea368ac8bd7eb6ca4dac20
CJ-HOME <cjk1805@naver.com>
2022-04-24 00:55:58 +0900

    더 맵게

---
+++ Programmers/MoreSpicy.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class MoreSpicy {
+    public static void main(String[] args) throws IOException
+    {
+        int[] scoville = {12, 2, 3, 9, 10, 2};
+        solution(scoville, 7);
+    }
+    public static int solution(int[] scoville, int K) {
+        int answer=0;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int val : scoville) {
+            pq.add(val);
+        }
+
+        while(pq.peek()<K){
+            int A = pq.poll();
+            int B = pq.poll();
+
+            int val = A + (B * 2);
+            pq.add(val);
+            answer++;
+        }
+        
+        return answer;
+    }
+}
